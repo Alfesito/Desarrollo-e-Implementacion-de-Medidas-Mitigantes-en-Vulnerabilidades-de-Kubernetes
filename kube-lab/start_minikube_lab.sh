b8b9ec2099f054fb50ff538facd195b4861c45b3
alfesito <andresalfe@gmail.com>
2024-03-23 11:16:10 +0100

    role for namespaces

--- kube-lab/start_minikube_lab.sh
+++ kube-lab/start_minikube_lab.sh
@@ -6,18 +6,19 @@ kubectl apply -f php-page.yaml
 kubectl create namespace back 2>/dev/null
 kubectl apply -f backend.yaml
 
-sleep 2
 pod_name=$(kubectl get pods -n back | grep -e mysql | awk '{print $1}')
 pod_status=$(kubectl get pods -n back "$pod_name" -o jsonpath='{.status.phase}')
+echo "$pod_status"
 
 while [ "$pod_status" != "Running" ]; do
-    sleep 1
+    sleep 3
     pod_status=$(kubectl get pods -n back "$pod_name" -o jsonpath='{.status.phase}' 2>/dev/null)
 done
 
-sleep 2
 kubectl cp ./docker-images/mysql/init.sql "$pod_name":/tmp/init.sql -n back
 kubectl exec -it "$pod_name" -n back -- bash -c 'mysql -u root -pp@ssword < /tmp/init.sql'
 kubectl exec -it "$pod_name" -n back -- rm /tmp/init.sql
 
-minikube service --all
+echo Fin
+
+#minikube service --all

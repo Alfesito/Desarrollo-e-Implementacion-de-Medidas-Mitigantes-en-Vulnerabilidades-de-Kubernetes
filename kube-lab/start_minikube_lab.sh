b447becc249eb9cff8eb2b479fc0abc3c5f023a2
Andrés Alfaro Fernández <andresalfe@gmail.com>
2024-04-03 10:22:10 +0200

    script update

--- kube-lab/start_minikube_lab.sh
+++ kube-lab/start_minikube_lab.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-minikube start
+read -p "¿Vas a hacer uso Minikube? (s/n): " is_minikube
+
+if [ "$is_minikube" == "s" ]; then
+    minikube start
+    minikube_status=$?
+    if [ $minikube_status -ne 0 ]; then
+        echo "Error al iniciar Minikube. Saliendo del script."
+        exit 1
+    fi
+fi
+
 kubectl apply -f grafana.yaml
 kubectl apply -f php-page.yaml
 kubectl create namespace back 2>/dev/null
@@ -15,10 +25,23 @@ while [ "$pod_status" != "Running" ]; do
     pod_status=$(kubectl get pods -n back "$pod_name" -o jsonpath='{.status.phase}' 2>/dev/null)
 done
 
-kubectl cp ./docker-images/mysql/init.sql "$pod_name":/tmp/init.sql -n back
-kubectl exec -it "$pod_name" -n back -- bash -c 'mysql -u root -pp@ssword < /tmp/init.sql'
-kubectl exec -it "$pod_name" -n back -- rm /tmp/init.sql
+kubectl cp ./docker-images/mysql/init.sql "$pod_name":/tmp/init.sql -n back 2>/dev/null
+kubectl exec -it "$pod_name" -n back -- bash -c 'mysql -u root -pp@ssword < /tmp/init.sql' 2>/dev/null
+kubectl exec -it "$pod_name" -n back -- rm /tmp/init.sql 2>/dev/null
 
 echo Fin
 
-#minikube service --all
+if [ "$is_minikube" == "s" ]; then
+    minikube service --all
+else
+    php_page_ip="<pending>"
+    php_page_port=""
+    
+    while [ "$php_page_ip" == "<pending>" ]; do
+        php_page_ip=$(kubectl get svc php-page -o=jsonpath='{.status.loadBalancer.ingress[0].ip}')
+        php_page_port=$(kubectl get svc php-page -o=jsonpath='{.spec.ports[0].nodePort}')
+        sleep 1
+    done
+
+    echo "La IP externa del servicio php-page es: $php_page_ip:$php_page_port"
+fi

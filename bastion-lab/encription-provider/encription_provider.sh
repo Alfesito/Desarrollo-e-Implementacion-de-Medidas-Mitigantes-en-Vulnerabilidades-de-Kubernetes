940574e6f99f440dc45d95b686d11eaaa2ae72b8
Andrés Alfaro Fernández <andresalfe@gmail.com>
2024-05-03 15:48:33 +0200

    bastion_lab big update

--- bastion-lab/encription-provider/encription_provider.sh
+++ bastion-lab/encription-provider/encription_provider.sh
@@ -19,13 +19,10 @@ resources:
       - identity: {}
 EOF
 dir_actual=$(pwd)
-pgrep -an kubelite | grep -oP -- '--apiserver-args-file=\K[^ ]+'
+#pgrep -an kubelite | grep -oP -- '--apiserver-args-file=\K[^ ]+'
 echo "--encryption-provider-config=$dir_actual/encryption_provider.yaml" >> /var/snap/microk8s/6641/args/kube-apiserver
 cd ..
 # Se reinicia kubelite para cargar la nueva configuración
 sudo systemctl restart snap.microk8s.daemon-kubelite
-while [[ $(systemctl is-active snap.microk8s.daemon-kubelite) != "active" ]]; do
-    sleep 1
-done
-
-kubectl get secrets --all-namespaces -o json | kubectl replace -f --all-namespaces -
+sleep 10
+#kubectl get secrets --all-namespaces -o json | kubectl replace -f --all-namespaces -

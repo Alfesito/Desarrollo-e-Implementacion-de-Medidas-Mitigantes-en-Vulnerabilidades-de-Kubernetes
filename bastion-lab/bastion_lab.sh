7bebd2f103a5834fc0465c54a3e4129cfec6a0ad
Andrés Alfaro Fernández <andresalfe@gmail.com>
2024-05-03 17:48:54 +0200

    bastion_lab big update

--- bastion-lab/bastion_lab.sh
+++ bastion-lab/bastion_lab.sh
@@ -11,9 +11,11 @@ sudo sed -i 's/--enable-admission-plugins=EventRateLimits/--enable-admission-plu
 # Aplicamos el encription provider y reiniciamos el servicio kube-apiserver
 sh ./encription-provider/encription_provider.sh
 # Creamos los distintos servicios y deployments, con su security context, para que no se ejecuten como root
+kubectl create namespace back 2>/dev/null
+kubectl apply -f backend-sec.yaml
 kubectl create namespace default 2>/dev/null
 kubectl apply -f grafana-sec.yaml
-kubectl apply -f php-page-sec.yamls
+kubectl apply -f php-page-sec.yaml
 kubectl create namespace back 2>/dev/null
 kubectl apply -f backend-sec.yaml
 
@@ -24,12 +26,8 @@ kubectl apply -f backend-sec.yaml
 kubectl label --overwrite ns default pod-security.kubernetes.io/enforce=restricted pod-security.kubernetes.io/enforce-version=v1.30
 kubectl label --overwrite ns back pod-security.kubernetes.io/enforce=baseline pod-security.kubernetes.io/enforce-version=v1.30
 
-pod_name=$(kubectl get pods -n back | grep -e mysql | awk '{print $1}')
-pod_status=$(kubectl get pods -n back "$pod_name" -o jsonpath='{.status.phase}')
-while [ "$pod_status" != "Running" ]; do
-    pod_status=$(kubectl get pods -n back "$pod_name" -o jsonpath='{.status.phase}')
-    sleep 1
-done
+sleep 10
+
 kubectl cp .init.sql "$pod_name":/tmp/init.sql -n back
 kubectl exec -it "$pod_name" -n back -- bash -c 'mysql -u root -pp@ssword < /tmp/init.sql'
 kubectl exec -it "$pod_name" -n back -- rm /tmp/init.sql

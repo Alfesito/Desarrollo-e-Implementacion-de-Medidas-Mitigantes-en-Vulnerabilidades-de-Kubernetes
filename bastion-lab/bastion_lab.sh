844b68eba4334ee37858c42c2f1332f35b9d57e3
Andrés Alfaro Fernández <andresalfe@gmail.com>
2024-05-08 01:02:23 +0200

    waf

--- bastion-lab/bastion_lab.sh
+++ bastion-lab/bastion_lab.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 # Este script está creado para su uso con microk8s, si se utiliza otra herramienta, no se puede asegurar su total funcionamiento
 
-snap start microk8s
-microk8s start
-alias kubectl='microk8s kubectl'
+#snap start microk8s
+#microk8s start
+#alias kubectl='microk8s kubectl'
+minikube start
+
+# Aplicamos el ingress security
+sh ./ingress-sec/gateway_waf.sh
+
 # Aplicamos el controlador de acceso AllwaysPullImages
 #pgrep -an kubelite | grep -oP -- '--apiserver-args-file=\K[^ ]+'
 #sudo sed -i 's/--enable-admission-plugins.*/--enable-admission-plugins=EventRateLimits/' /var/snap/microk8s/6641/args/kube-apiserver
-sudo sed -i 's/--enable-admission-plugins=EventRateLimits/--enable-admission-plugins=AllwaysPullImages/' /var/snap/microk8s/6641/args/kube-apiserver
+#sudo sed -i 's/--enable-admission-plugins=EventRateLimits/--enable-admission-plugins=AllwaysPullImages/' /var/snap/microk8s/6641/args/kube-apiserver
 # Aplicamos el encription provider y reiniciamos el servicio kube-apiserver
-sh ./encription-provider/encription_provider.sh
+#sh ./encription-provider/encription_provider.sh
 # Creamos los distintos servicios y deployments, con su security context, para que no se ejecuten como root
 kubectl create namespace back 2>/dev/null
 kubectl apply -f backend-sec.yaml
@@ -17,21 +22,23 @@ kubectl create namespace front 2>/dev/null
 kubectl apply -f grafana-sec.yaml
 kubectl apply -f php-page-sec.yaml
 
-# Aplicamos el ingress security
-#sh ./ingress-sec/ingress_sec.sh
-
-# Aplicamos el controlador de acceso PodSecurity
-kubectl label --overwrite ns default pod-security.kubernetes.io/enforce=restricted pod-security.kubernetes.io/enforce-version=v1.30
-kubectl label --overwrite ns back pod-security.kubernetes.io/enforce=baseline pod-security.kubernetes.io/enforce-version=v1.30
-
 mysql_pod_status=$(kubectl get pods -n back | grep "^mysql-" | awk '{print $3}')
-while [ "$mysql_pod_status" != "Running" ]; do
-    mysql_pod_status=$(kubectl get pods -n back | grep "^mysql-" | awk '{print $3}')
-    sleep 5
-done
+if [ "$mysql_pod_status" != "Running" ];then
+    while [ "$mysql_pod_status" != "Running" ]; do
+        mysql_pod_status=$(kubectl get pods -n back | grep "^mysql-" | awk '{print $3}')
+        sleep 1
+    done
+fi
 mysql_pod_name=$(kubectl get pods -n back | grep -e mysql | awk '{print $1}')
-kubectl cp ./docker-images/mysql/init.sql "$mysql_pod_name":/tmp/init.sql -n back
+kubectl exec -t "$mysql_pod_name" -n back -- bash -c 'apt-get install mysql-server'
+kubectl cp ./data/init.sql "$mysql_pod_name":/tmp/init.sql -n back
 kubectl exec -t "$mysql_pod_name" -n back -- bash -c 'mysql -u root -pp@ssword < /tmp/init.sql'
 kubectl exec -t "$mysql_pod_name" -n back -- rm /tmp/init.sql
 
-echo "La IP externa del servicio php-page es: $(curl ifconfig.me):80"
+# Aplicamos el controlador de acceso PodSecurity
+#kubectl label --overwrite ns default pod-security.kubernetes.io/enforce=restricted pod-security.kubernetes.io/enforce-version=v1.30
+#kubectl label --overwrite ns back pod-security.kubernetes.io/enforce=baseline pod-security.kubernetes.io/enforce-version=v1.30
+
+kubectl create ingress php-localhost --class=nginx --rule='php.internal/*=php-page:80' -n front
+echo "La IP externa del servicio php-page es: http://php.internal:8080"
+kubectl port-forward --namespace=ingress-nginx service/ingress-nginx-controller 8080:80
---
+++ bastion-lab/ingress-sec/gateway_waf.sh
@@ -0,0 +1,22 @@
+cd ingress-sec
+# Desplegamos el gateway
+kubectl apply -f deploy_waf.yaml
+sleep 10
+
+# Creamos los certificados TLS
+#openssl req -x509 -nodes -days 1000 -newkey rsa:2048 -keyout ingress.key -out ingress.crt -subj "/CN=php.internal/O=security"
+#kubectl create secret tls tls-cert --key ingress.key --cert ingress.crt
+#kubectl apply -f ingress-https.yaml
+
+# Escribir en /etc/hosts la ip y el dominio
+#ip=$(hostname -I | awk '{print $1}')
+ip=127.0.0.1
+if grep -q "php.internal" /etc/hosts; then
+    echo "Ya está el dominio php.internal en /etc/hosts"
+else
+    sudo sh -c 'echo "'$ip'  php.internal" >> /etc/hosts'
+    echo "Se ha añadido el dominio php.internal a /etc/hosts"
+fi
+
+cd ..
+kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=120s
